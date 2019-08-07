fe82c021df48d0741b3b09273a9592e504c1ba2d
unknown <jtang47@illinois.edu>
2019-08-07 11:13:22 -0400

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { WebcallService } from './webcall.service';
+import { Observable, observable } from 'rxjs';
+import { getRandomString } from 'selenium-webdriver/safari';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Another';
+  @Output() feedback = [];
+  constructor(private webService: WebcallService) {
+
+  }
+
+  hide(){
+    document.getElementById('carousal').style.display = 'hide';
+  }
+
+  display(i: number){
+    document.getElementById('carousal').innerHTML = '<img src=' + this.feedback[i].images[0].href + '>';
+    console.log(i);
+  }
+
+  ngOnInit() {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+    this.webService.getData().subscribe(data =>
+      {this.feedback = data.groups;
+       console.log(this.feedback);
+    }
+    );
+
+
+  }
 }
+
